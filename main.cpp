98f5de4aadb5e3290d0d9776a20a76f0dd1a36a1
Raoul Wols <raoulwols@gmail.com>
2020-12-31 14:24:25 +0100

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,14 @@
+/* Apache License, Version 2.0 */
+
+#include <fstream>
+#include <gtest/gtest.h>
+#include <ios>
+#include <memory>
+#include <sstream>
+#include <string>
+#include <system_error>
+
+#include "testing/testing.h"
+const auto t = 1l;
+
+int main() { return 0; }
